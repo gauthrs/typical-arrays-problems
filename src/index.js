8b31a969d9fcdc8df081fe33ab097bc2f7329523
Gauthier <gotyeme@gmail.com>
2020-10-01 13:18:07 +0300

    feat: added solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((x, y) => x + y, 0) / array.length;
 }
